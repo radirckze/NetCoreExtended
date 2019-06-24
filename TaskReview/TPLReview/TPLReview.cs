f08587fa36b8e5099279070e5f167362a398dc1e
Ravi Dirckze <r_dirckze@yahoo.com>
2019-06-23 18:18:17 -0700

    restructued code

--- TaskReview/TPLReview/TPLReview.cs
+++ TaskReview/TPLReview/TPLReview.cs
@@ -45,29 +45,47 @@ namespace TaskReview
         public void Run()
         {
 
-            Console.WriteLine("Starting TaskBasics ...");
+            Console.WriteLine("Starting Task Parallel Library (TPL) review ...");
 
-            bool runInitiationPatterns = true;
-            if (runInitiationPatterns) 
+            bool runTaskInstantiationOptions = true;
+            if (runTaskInstantiationOptions) 
             {
-                //There are 3 primary task initiations patterns, ie., new Task followed by Start, Task.Run and 
+
+                //Tasks can be created implicitly (i.e., Parallel.Invoke) or explicitly (see below).
+
+                //Creating tasks implicitly
+                Parallel.Invoke(() => Console.WriteLine("Implicit taks creation - Parallel.Invoke called."));
+                Parallel.Invoke(() => PrintMsgAction("Implicit taks creation - Parallel.Invoke called with Action ..."));
+                //You can create and run multiple tasks (in parallel) in same invoke statement
+                Parallel.Invoke
+                (
+                    () => AddIntegers(3,5),
+                    () => PrintMsgAction("Implicit taks creation - Parallel.Invoke with multiple tasks")
+                );
+
+                //The three options for instantiating a task explicitly, i.e., new Task followed by Start, Task.Run and 
                 // Task.Facory.StartNew
+                Task ePattern1 = new Task(() => PrintMsgAction("Explicit task instantiation - new Task ...")); 
+                ePattern1.Start();
+                Task ePattern2 = Task.Run(() => PrintMsgAction("Explicit task instantiation - Task.Run ..."));                
+                Task ePattern3 = Task.Factory.StartNew(() => PrintMsgAction("Explicit task instantiation - Task.Facory.StartNew ..."));
+                Task.WaitAll(ePattern1, ePattern2, ePattern3);
+                //Note, Task.Run and Task.Factory.StartNew are convenience methods. For example, Task.Run uses the default scheduler, 
+                //and the StartNew is used when you want to create and start in the same line of code
                 
-                // new task ...
-                Console.WriteLine("Runninng task initiation patterns ...");
-                Task ipTask1A = new Task(() => PrintMsgAction("Task 1A - PrintMsgAction version")); //using new task (Action delegate) 
-                Task ipTask1B = new Task(() => PrintMsg("Task1B - PrintMsg method version"));  //using new task (method delegate)           
-                Task ipTask1C = new Task(() => Console.WriteLine("Task1C  - inline expression version")); //using new task inline version
+                //When creating a task, the delegate can be a named delegate, an anonymous method or a lambda expression
+                Task dPattern1A = new Task(() => PrintMsgAction("Task with named action")); //using named (Action() delegate 
+                Task dPattern1B = new Task(() => PrintMsg("Task with named method"));  //using named method 
+                Task dPattern2 = new Task(() => {Console.WriteLine("Task with anonymous method");});          
+                Task dPattern3 = new Task(() => Console.WriteLine("Task with lambda expression")); //using lambda expression
                 
-                ipTask1A.Start();
-                ipTask1B.Start();
-                ipTask1C.Start();
+                dPattern1A.Start();
+                dPattern1B.Start();
+                dPattern2.Start();
+                dPattern3.Start();
+                Task.WaitAll(dPattern1A, dPattern1B, dPattern2, dPattern3);
 
-                //RunSynchronously and Async
-
-                Task.WaitAll(ipTask1A, ipTask1B, ipTask1C);
-
-                // StartNew(...) gives us more task options
+                //Example: using  StartNew to pass in state
                 List<Task> statefulTasks = new List<Task>();
                 for(int i=0; i<5; i++)
                 {
@@ -88,14 +106,9 @@ namespace TaskReview
                     Console.WriteLine(String.Format("[Stateful task execution info] Name = {0}, thread = {1}, status = {2}", state.Name, state.ThreadNum, task.Status));
                 }
 
-                // Task Run option .... (Light weight version of Task.StartNew)
-                Task ipTask2A = Task.Run(() => {Console.WriteLine("Lambda expression ipTask2");}); //
-
-                Task.WaitAll(ipTask2A);
-
                 
 
-                //Specifying task scheduling options
+                //Example: Specifying task scheduling options
                 //Available options None, PreferFairness (scheduling), LongRunning, AttachdToParent (for sub-tasks), ...
                 Task rcoTask = new Task(() => PrintMsg("runCreationOptions task running ..."), TaskCreationOptions.LongRunning | TaskCreationOptions.PreferFairness);
                 rcoTask.Start();
@@ -103,9 +116,27 @@ namespace TaskReview
 
             }
 
-             bool runTaskParemeter = false;
-            if (runTaskParemeter)
+            bool runTaskParamsAndResult = false;
+            if (runTaskParamsAndResult)
             {
+                 Task<int> rtTask1 = Task.Factory.StartNew(() => ReturnSum(1,2));
+                int result = rtTask1.Result + Task<int>.Run(() => ReturnSum(3,4)).Result; // calling task.Result will block
+                Console.WriteLine(String.Format("Running task result .... 1+2+3+4 = {0}", result));
+
+                // For more details on returning a result from a task see 
+                // https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/how-to-return-a-value-from-a-task
+
+                // Note: Task.Result should be avoided when using async as this could result in a deadlok. More on this later.
+            }
+
+            bool runTaskCustomization = false;
+            if (runTaskCustomization)
+            {
+
+                //Task.StartNew provides more control such as, specifying the scheduler, providing state, etc.
+                //TBD
+
+
                  string w = "";
                 Task temp = new Task((o) => {Console.WriteLine(0);}, w);
 
@@ -123,15 +154,26 @@ namespace TaskReview
 
             } 
 
-            bool runParallel = false;
-            if (runParallel)
+            bool runTaskCulture = false;
+            if (runTaskCulture)
             {
-                Parallel.Invoke(() => AddIntegers(3,5), () => {Console.WriteLine("2nd task running in parallel");} );
+                //Unless you have sepecified the culture for all threads using  CultureInfo.DefaultThreadCurrentCulture, each
+                //thread gets its culture from the System Culture. (Note that a new thread does not get its culture from the
+                //the thread that created it. )
+
+                //Starting from .NET 4.6 onwards each task inherits its culture from it's calling thread.
             }
 
-            bool runTaskCancellation = false;
-            if (runTaskCancellation) {
+            bool runTaskContinue = false;
+            if (runTaskContinue)
+            {
+                //Task<int, int> squareTask = new Task<int>(() => {return 5*5;});
+            }
 
+            bool runTaskSuncAndManagement = false;
+            if (runTaskSuncAndManagement)
+            {
+                //Task cancellation ....
                 Console.WriteLine("Runninng task cancellation ...");
                 CancellationTokenSource cts = new CancellationTokenSource();
                 Task cancelTask = new Task(() => PrintMsgAction("Action Task 1"), cts.Token);
@@ -148,39 +190,17 @@ namespace TaskReview
                 }
 
                 Console.WriteLine(String.Format("Task Cancellation completed. Task status = {0}, IsCancelled = {1}", cancelTask.Status, cancelTask.IsCanceled));
-            }
 
-            bool runTestWait = false;
-            if (runTestWait) 
-            {
-                Console.WriteLine("Runninng task wait ..."); 
+                //Taks wait
+                 Console.WriteLine("Runninng task wait ..."); 
                 Task.Run(() => PrintMsg("Task 1")).Wait(); // will wait before moving on to next line of code
                 Task task2 = Task.Run(() => PrintMsg("Task 2")); // will start task and move on to next line of code
                 Task task3 = Task.Run(() => PrintMsg("Task 3")); // will start task and move on to next line of code               
                 Task.WaitAll(task2, task3);
 
                 Console.WriteLine(String.Format("Tasks wait completed. Task2 status = {0}, Task3 status = {1}", task2.Status, task3.Status));
-            }
 
-            bool runTaskResult = false;
-            if (runTaskResult)
-            {
-                Task<int> rtTask1 = Task.Factory.StartNew(() => ReturnSum(1,2));
-                int result = rtTask1.Result + Task<int>.Run(() => ReturnSum(3,4)).Result; // calling task.Result will block
-                Console.WriteLine(String.Format("Running task result .... 1+2+3+4 = {0}", result));
-
-                // For more details on returning a result from a task see 
-                // https://docs.microsoft.com/en-us/dotnet/standard/parallel-programming/how-to-return-a-value-from-a-task
-
-                // Note: Task.Result should be avoided when using async as this could result in a deadlok. More on this later.
-            }
-
-            bool runTaskContinuations = false;
-            if (runTaskContinuations)
-            {
-                
-                
-               
+                //RunSynchronously and Async
             }
 
             Console.WriteLine("Completed TaskBasics.");
