1538542078839c954710f3d449f2852fdf34d0d9
Ravi Dirckze <r_dirckze@yahoo.com>
2019-07-01 19:59:08 -0700

    updated

---
+++ TaskReview/TAP/Tap.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+// Tasked based Anync Pattern review
+
+namespace TAP
+{
+
+    //review notes 
+    //An asynchronous method should minimize the synchronous work it needs to do.
+    //An asynchronous method should raise an exception to be 
+    //thrown out of the asynchronous method call only in response to a usage error. 
+    //[TBD] reporting progress, cancellation, etc.
+
+}
